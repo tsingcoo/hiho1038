cbe1a5abbbcd2bcfc0580f2b6e56ca07c168cc59
wangqinglong <tsingcoo.wang@gmail.com>
2017-02-19 13:34:26 +0800

    采用了新型的存储记录方式，使得原来的需要2*M长数组的空间减少为只需要M长数组空间即可

--- main.cpp
+++ main.cpp
@@ -27,7 +27,27 @@ int DP(std::vector<int> &need, std::vector<int> &value, int N, int M) {
 
     return std::max(q[M], p[M]);
 
+}
 
+int DP2(std::vector<int> &need, std::vector<int> &value, int N, int M) {
+    std::vector<int> f(M + 1, 0);
+    for (int i = 0; i < N; ++i) {//N件商品
+        for (int j = M; j >= 0; --j) {
+            if (j >= need[i]) {
+                f[j] = std::max(f[j - need[i]] + value[i], f[j]);
+                //曾经错误的方式，因为下面的方式我没有办法保证每一次f[j]都比f[j-need[i]]+value[i]小
+                //f[j] = f[j - need[i]] + value[i];
+            }
+//            else {
+//                f[j] = f[j];
+//            }
+        }
+    }
+//    for (auto i = f.begin(); i != f.end(); ++i) {
+//        std::cout << *i << " ";
+//    }
+//    std::cout << std::endl;
+    return f[M];
 }
 
 int main() {
@@ -51,11 +71,11 @@ int main() {
         value.push_back(v);
     }
 
-    int res;
-    res = DP(need, value, count, money);
+    int res1, res2;
+//    res1 = DP(need, value, count, money);
+    res2 = DP2(need, value, count, money);
 
-
-    std::cout << res << std::endl;
-
+//    std::cout << res1 << std::endl;
+    std::cout << res2 << std::endl;
     return 0;
 }
